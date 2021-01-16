b344dab5bdc99be08f5971234919d0b9fe4fb6ca
Yash Rohatgi <rohatgiy@gmail.com>
2021-01-16 10:47:36 -0500

    added imports

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
-import tensorflow
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+import requests
+
+def main():
+    print('hi')
+
+
+if __name__ == "__main__":
+    main()
