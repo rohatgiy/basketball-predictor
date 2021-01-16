f535abeba0501918a4ae8b1eadcfe830bfccdcff
Yash Rohatgi <rohatgiy@gmail.com>
2021-01-16 11:01:34 -0500

    started parsing

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 import requests
+import json
 
 # http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2020/league/00_full_schedule.json
 # http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2019/league/00_full_schedule.json
@@ -9,7 +10,8 @@ import requests
 
 def main():
     res = requests.get("http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2019/league/00_full_schedule.json")
-    print(res.json())
+    obj = res.json()
+    for _ in range(len(obj[])
 
 
 if __name__ == "__main__":
