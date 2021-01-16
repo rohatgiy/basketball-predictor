4ac391d023a833783e9dd518263320354431c3c4
Kevin Lee <kevin348960@gmail.com>
2021-01-16 11:01:02 -0500

    team abbrv list

--- model.py
+++ model.py
@@ -6,6 +6,8 @@ import requests
 # http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2020/league/00_full_schedule.json
 # http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2019/league/00_full_schedule.json
 
+teamAbbrvs = ["ATL", "BKN", "BOS", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", 
+"LAC", "LAL", "MEM", "MIA", "MIL", "NOP", "NYK", "OKC", "PHI", "PHX", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
 
 def main():
     res = requests.get("http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2019/league/00_full_schedule.json")
