4a9b81283c86c84a671874d1763a305a338b9ecb
Yash Rohatgi <rohatgiy@gmail.com>
2021-01-16 10:49:42 -0500

    added links

--- model.py
+++ model.py
@@ -3,7 +3,12 @@ import numpy as np
 from tensorflow import keras
 import requests
 
+# http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2020/league/00_full_schedule.json
+# http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2019/league/00_full_schedule.json
+
+
 def main():
+    req = requests.get()
     print('hi')
 
 
