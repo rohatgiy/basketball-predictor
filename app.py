50f524b3badfd9a967525b0d311b94b558d995e4
Yash Rohatgi <rohatgiy@gmail.com>
2021-01-17 03:25:24 -0500

    added routes

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return "this is index"
+
+@app.route('/predict', methods=["GET"])
+def predict():
+    return "predict here"
+
+@app.route('/results', methods=["POST"])
+def results():
+    return requestl.form
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for('index'))
